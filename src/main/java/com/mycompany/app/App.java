b2b715460bb605b8be2d3f0cf973d9e5122052b3
vams-cmd <81565954+vams-cmd@users.noreply.github.com>
2022-01-28 15:42:52 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,20 +2,28 @@ package com.mycompany.app;
 
 /**
  * Hello world!
+ *
  */
-public class App
+public class App 
 {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! --> This is my 1st Assignment" );
 
-    private final String message = "Hello World! --> this is my Assignment one";
-
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+        System.out.println("A NullPointerException is a drag...");
+        Object myObject = null;
+        /**
+         * Checker prevents this from compiling...
+         */
+//        System.out.println("myObject: " + myObject.toString());
+        /**
+         * ... which is simply fantastic. It shows:
+         *
+         * error: [dereference.of.nullable] dereference of possibly-null
+         * reference myObject
+         *
+         * http://checkerframework.org
+         */
+        System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
     }
-
-    private final String getMessage() {
-        return message;
-    }
-
 }
