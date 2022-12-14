764da3a64de06790a52bcf6f262c59c2446831d5
vamsi <vamsi@gmail.com>
2022-12-14 12:20:46 +0530

    bug

--- samplebug.java
+++ samplebug.java
@@ -1,1 +1,22 @@
-
+@Override
+public boolean equals(Object o) 
+{
+  ....
+  CheckpointStatistics that = (CheckpointStatistics) o;
+  return id == that.id &&
+    savepoint == that.savepoint &&
+    triggerTimestamp == that.triggerTimestamp &&
+    latestAckTimestamp == that.latestAckTimestamp &&
+    stateSize == that.stateSize &&
+    duration == that.duration &&
+    alignmentBuffered == that.alignmentBuffered &&
+    processedData == processedData &&                // <=
+    persistedData == that.persistedData &&
+    numSubtasks == that.numSubtasks &&
+    numAckSubtasks == that.numAckSubtasks &&
+    status == that.status &&
+    Objects.equals(checkpointType, that.checkpointType) &&
+    Objects.equals(
+      checkpointStatisticsPerTask, 
+      that.checkpointStatisticsPerTask);
+}
